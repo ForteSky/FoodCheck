e06d4050425593a9bc4b779c6b2c7a1fb3d9279d
ForteSky <nukle.jp@gmail.com>
2022-05-29 13:06:46 +0100

    Initial commit

---
+++ SamplePlugin/Plugin.cs
@@ -0,0 +1,81 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using System.Reflection;
+using Dalamud.Game.ClientState.Party;
+using Dalamud.Game.Gui;
+
+namespace SamplePlugin
+{
+    public sealed class Plugin : IDalamudPlugin
+    {
+        public string Name => "Sample Plugin";
+
+        private const string commandName = "/pmycommand";
+        
+        [PluginService]
+        internal static PartyList PartyList { get; private set; }
+        
+        [PluginService]
+        internal static ChatGui ChatGui { get; private set; }
+        private DalamudPluginInterface PluginInterface { get; init; }
+        private CommandManager CommandManager { get; init; }
+        private Configuration Configuration { get; init; }
+        private PluginUI PluginUi { get; init; }
+
+        public Plugin(
+            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+            [RequiredVersion("1.0")] CommandManager commandManager)
+        {
+            this.PluginInterface = pluginInterface;
+            this.CommandManager = commandManager;
+
+            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+            this.Configuration.Initialize(this.PluginInterface);
+
+            // you might normally want to embed resources and load them from the manifest stream
+            var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
+            var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
+            this.PluginUi = new PluginUI(this.Configuration, goatImage);
+
+            this.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "A useful message to display in /xlhelp"
+            });
+
+            this.PluginInterface.UiBuilder.Draw += DrawUI;
+            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+        }
+
+        public void Dispose()
+        {
+            this.PluginUi.Dispose();
+            this.CommandManager.RemoveHandler(commandName);
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            // this.PluginUi.Visible = true;
+            // Dalamud.Game.ClientState.Party.PartyMember
+            
+            foreach (var partyMember in PartyList)
+            {
+                ChatGui.Print(partyMember.Name);
+            }
+            ChatGui.UpdateQueue();
+            
+        }
+
+        private void DrawUI()
+        {
+            this.PluginUi.Draw();
+        }
+
+        private void DrawConfigUI()
+        {
+            this.PluginUi.SettingsVisible = true;
+        }
+    }
+}
